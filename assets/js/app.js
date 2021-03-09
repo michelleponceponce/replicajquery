e0ae74b577005634b0496b69fd3da4302f524b8e
michelleponceponce <62112590+michelleponceponce@users.noreply.github.com>
2021-03-09 10:57:36 -0600

    Initial commit

---
+++ assets/js/app.js
@@ -0,0 +1,13 @@
+
+function changeColor()
+ {
+    document.getElementById('circulo').style.backgroundColor = 'Red';  
+    setTimeout("ChangeColor4()", 2000);  
+}
+
+
+
+function ChangeColor4()  
+               {  
+                    document.getElementById('circulo').style.backgroundColor='blue';  
+               }  
